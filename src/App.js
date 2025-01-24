06580034e8588b574ea9d07460cb846a3aaae34d
rosemolt <rosemolthomas02@gmail.com>
2025-01-24 19:18:31 +0530

    Awareness created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
+import Awareness from './components/Awareness';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<HomePage/>} />
+          <Route path="/awareness" element={<Awareness/>} />
         </Routes>
       </div>
     </BrowserRouter>
